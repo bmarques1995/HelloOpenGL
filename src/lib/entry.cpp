ca67b7e9722af394e347d9639f0f9df0c9808e77
bmarques1995 <brunosmarques1995@hotmail.com>
2022-12-09 09:41:10 -0300

    Added basic window

---
+++ src/lib/entry.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <windows.h>
+
+#include <glfw/glfw3.h>
+
+INT APIENTRY wWinMain(_In_ HINSTANCE hInstance, 
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR lpCmdLine,
+    _In_ int nCmdShow)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
