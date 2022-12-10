c89fba8ef9e1692cd07ee9d29a69ed47d47c6305
B Marques <brunosmarques1995@hotmail.com>
2022-12-10 14:38:18 -0300

    Added linux entrypoint

--- src/lib/entry.cpp
+++ src/lib/entry.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
-#include <windows.h>
-
 #include <glfw/glfw3.h>
+
+#ifdef WIN_OPEN_GL
+
+#include <windows.h>
 
 INT APIENTRY wWinMain(_In_ HINSTANCE hInstance, 
     _In_opt_ HINSTANCE hPrevInstance,
@@ -39,3 +41,44 @@ INT APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     glfwTerminate();
     return 0;
 }
+
+#elif LIN_OPEN_GL
+
+int main()
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+#endif
+
+
+
