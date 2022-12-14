e0d83ef00992e4b73747b0e6f1b62626ffbaa271
bmarques1995 <brunosmarques1995@hotmail.com>
2022-12-14 09:24:40 -0300

    Added empty window

--- src/lib/entry.cpp
+++ src/lib/entry.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 #include <windows.h>
 
+#include <glad/gl_safe_include.h>
 #include <glfw/glfw3.h>
+
+int program();
 
 INT APIENTRY wWinMain(_In_ HINSTANCE hInstance, 
     _In_opt_ HINSTANCE hPrevInstance,
     _In_ LPWSTR lpCmdLine,
     _In_ int nCmdShow)
+{
+    return program();
+}
+
+int program()
 {
     GLFWwindow* window;
 
@@ -25,10 +33,17 @@ INT APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    int status = gladLoadGL(glfwGetProcAddress);
+
+    if (status == 0)
+        return -2;
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
+        glClearColor(.7,.3,.9,1.0f);
 
+        glClear(GL_COLOR_BUFFER_BIT);
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
